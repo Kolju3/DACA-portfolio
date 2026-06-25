4430bda8cbd545ae746bba4500a93ef373bfdc21
Kolju3 <kolju@gmail.com>
2026-06-25 18:58:45 +0300

    Lisatud kommentaarid

--- Week1/Puuduvad_andmed.sql
+++ Week1/Puuduvad_andmed.sql
@@ -1,4 +1,4 @@
-SELECT COUNT (*) - COUNT (first_name) AS "Puuduvad_eesnimed",
-         COUNT (*) - COUNT (last_name) AS "Puuduvad_perekonnanimed",
-         COUNT (*) - COUNT (email) AS "Puuduvad_emailid"
+SELECT COUNT (*) - COUNT (first_name) AS "Puuduvad_eesnimed", --SELECT valib siin kõik klientide read ja siis lahutab neist maha kõik, kellel on eesnimi, et saada puuduvate eesnimede arv.
+         COUNT (*) - COUNT (last_name) AS "Puuduvad_perekonnanimed", --SELECT valib siin kõik klientide read ja siis lahutab neist maha kõik, kellel on perekonnanimi, et saada puuduvate perekonnanimede arv.
+         COUNT (*) - COUNT (email) AS "Puuduvad_emailid" --SELECT valib siin kõik klientide read ja siis lahutab neist maha kõik, kellel on email, et saada puuduvate emailide arv.
 FROM "Customers";
