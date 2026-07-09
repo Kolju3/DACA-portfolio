321358fd76f90a2ffca6bd85094ee9b7a5ed892a
Kolju3 <kolju@gmail.com>
2026-07-09 14:56:50 +0300

    Organized my git Portfolio

---
+++ Week 3/Week_3_README.md
@@ -0,0 +1,10 @@
+# Nädal 3 – kalju individuaalne töö
+
+## Ülesanne
+_Täienda ise_
+
+## Mida tegin
+- _Lisa siia_
+
+## SQL päringud / failid
+_Lisa lingid_
--- Week1/REgistratsiooni_ajad.sql
+++
@@ -1,3 +0,0 @@
-SELECT MIN (registration_date) AS "Esimene registratsiooni kuupäev",
-MAX (registration_date) AS "Viimane registratsiooni kuupäev"
-FROM "Customers"
